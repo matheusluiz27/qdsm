39d176ff7c4998d0b89ef145d86576256d694d5a
Matheus Luiz <matheusluiz27@gmail.com>
2021-03-24 00:58:33 -0300

    Criando tela patient result

---
+++ qdsm-web/src/app/patient-result/patient-result.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PatientResultComponent } from './patient-result.component';
+
+describe('PatientResultComponent', () => {
+  let component: PatientResultComponent;
+  let fixture: ComponentFixture<PatientResultComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PatientResultComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PatientResultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
